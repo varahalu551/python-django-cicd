68d3c0cb8b40e4132449209a0b3372b8a4579e5b
varahalu51 <nagam.svb@gmail.com>
2024-10-14 12:40:39 +0530

    add site available

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo cp -rf app.conf  /etc/nginx/conf.d
+sudo cp -rf app.conf /etc/nginx/sites-available/app
 chmod 710  /var/lib/jenkins/workspace/django-cicd
-
+sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled/
 sudo nginx -t
 
 sudo systemctl start nginx
