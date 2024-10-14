677545d647e54e49f2b78618f386a452e37f3704
varahalu51 <nagam.svb@gmail.com>
2024-10-14 10:55:52 +0530

    replace code in envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-if [-d "env"]
-then 
-echo "Python virtual env exists"
-  python -m venv env
+
+if [ -d "env" ]; then 
+    echo "Python virtual env exists"
+    python -m venv env
 fi
+
 echo $PWD
-source env/Scripts/activate
+source env/bin/activate
 
-if [-d "logs"]
-then 
-  echo "log folder exists"
+if [ -d "logs" ]; then 
+    echo "log folder exists"
 else
-   mkdir logs
-   touch logs/error.log logs/access.log
-
+    mkdir logs
+    touch logs/error.log logs/access.log
 fi
 
+# Use this if passwordless sudo is set up, or provide password with `-S`
 sudo chmod -R 777 logs
-echo   ""envsetup finishes"
+
+echo "envsetup finishes"
